087f2bceb2347259f7946d0afc2233d5e75bff18
github username <github email address>
2019-12-18 01:48:33 +0000

    changed

--- src/components/sites/SiteDetailsHeader.js
+++ src/components/sites/SiteDetailsHeader.js
@@ -4,7 +4,7 @@ import React from "react";
     console.log(props)
     const checkData = props.sites.length === 0;
     console.log(checkData)
-    return (checkData ? <div><h3>No Site Available</h3></div> : <div><h3>Click a site from the list above for more details</h3></div>)
+    return (checkData ? <h3>No Site Available</h3> : <h3>Click a site from the list above for more details</h3>)
 }
 
 export { SiteDetailsHeader }
