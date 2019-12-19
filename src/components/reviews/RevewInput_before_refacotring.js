571ab654f4475330bb1318c51e89ea650d4a4959
github username <github email address>
2019-12-19 11:38:13 +0000

    changed to method instead of const in render ()

--- src/components/reviews/RevewInput_before_refacotring.js
+++ src/components/reviews/RevewInput_before_refacotring.js
@@ -36,13 +36,11 @@ class ReviewInput extends Component {
         })
       }
 
-    renderOptions = () => {
-        const options = [{rating:1}, {rating:2}, {rating:3}, {rating:4}, {rating:5}];
-        return options.map(option => (<option key={option.rating} value={option.rating}>{option.rating}</option>));
-    }
-
     render () { 
         console.log(this.props)
+        const options = [{rating:1}, {rating:2}, {rating:3}, {rating:4}, {rating:5}];
+        const renderOptions = options.map(option => (<option key={option.rating} value={option.rating}>{option.rating}</option>));
+
       return( 
             <div>
           <br/>
@@ -50,7 +48,7 @@ class ReviewInput extends Component {
             <form onSubmit={this.handleOnSubmit} className="form_style"> 
                 <label>Comment: <textarea name="comment" onChange={this.handleTextareaOnChange} type="text" className="textarea" value={this.state.comment}/></label>
                 <br/>
-                <label>Rating: <select name ="rating" onChange={this.handleSelectOnChange} value={this.state.rating}>{this.renderOptions()}</select></label>
+                <label>Rating: <select name ="rating" onChange={this.handleSelectOnChange} value={this.state.rating}>{renderOptions}</select></label>
                 <br/>
                 <input type="submit" value="Add"/>
             </form>
