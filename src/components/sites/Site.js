f089ecce03b7ebbf1aaa5ae0690519c93edb04f3
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-02-22 19:01:35 +0000

    changed

--- src/components/sites/Site.js
+++ src/components/sites/Site.js
@@ -3,7 +3,7 @@ import React from "react";
 const siteDetails = (props) => {
     console.log(props)
       return (<div>
-      <h3>Site Detrails</h3>
+      <h3>Site Details</h3>
       <h4>Site Name: {props.site.site}</h4>
       <h4>Short Description:</h4><p>{props.site.short_description}</p>
       <h4>Official UNESCO website: </h4><a href={props.site.http_url} key={props.site.id_number} rel="noopener noreferrer" target="_blank"> {props.site.http_url}</a>
